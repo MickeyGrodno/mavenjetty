e698ad773ec374935ddaef9f0a0b648a1eedb611
Sergei <31308952+MickeyGrodno@users.noreply.github.com>
2018-05-08 21:51:01 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import org.eclipse.jetty.server.Server;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Frontend frontend = new Frontend();
+        Server server = new Server(8080);
+        ServletContextHandler
+    }
+}
